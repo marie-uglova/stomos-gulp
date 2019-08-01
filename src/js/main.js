22b8947cc0bae47f778903618a6e8e77fd8d19f2
marie-uglova <53574666+marie-uglova@users.noreply.github.com>
2019-08-01 21:24:43 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	
+	$('.mobile-menu-burger').click(function(){
+		$('.mobile-menu').toggleClass('act');
+		return false;
+	});
+	$('.search-call,.close-me').click(function(){
+		$('.search-block').toggleClass('active');
+		return false;
+	});
+	$('.colorbox').colorbox({rel:'gal'});
+	jQuery.colorbox.settings.maxWidth  = '95%';
+	jQuery.colorbox.settings.maxHeight = '95%';
+	var resizeTimer;
+	function resizeColorBox()
+	{
+		if (resizeTimer) clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+				if (jQuery('#cboxOverlay').is(':visible')) {
+						jQuery.colorbox.load(true);
+				}
+		}, 300);
+	}
+	jQuery(window).resize(resizeColorBox);
+	window.addEventListener("orientationchange", resizeColorBox, false);
+	
+});
